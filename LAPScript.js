bcdeb3459014b7220f03477699dc29f5195c567a
Ashley Callahan <info@ashleycallahan.net>
2026-05-22 15:30:14 -0400

    Adjustments to accommodate screens without images and tenants without tokens

--- LAPScript.js
+++ LAPScript.js
@@ -77,12 +77,12 @@
     }
     window.addScreenImg = function() {
         for (var i = 0; i < window.screens.length; i++) {
-            if (typeof window.screens[i].page !== 'undefined' && typeof window.screens[i].page.name !== 'undefined' && typeof window.screens[i].page.screenshot.src !== 'undefined') {
+            if (typeof window.screens[i].page !== 'undefined' && typeof window.screens[i].page.name !== 'undefined') {
                 var pageLink = $('app-manual-evaluation-screens-evaluated a[routerlink]:contains(' + window.screens[i].page.name + ')').first();
                 if (pageLink.length > 0 && pageLink.next('.review-mode-new-tab-link-added').length === 0) {
                     pageLink.parent().append('<a href="' + pageLink.attr('href') + '" target="_blank" class="review-mode-new-tab-link-added" style="margin:0 -26px 0 10px"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" role="img" aria-label="Open in a new tab" style="width:16px;height:16px"><path d="M354.4 83.8C359.4 71.8 371.1 64 384 64L544 64C561.7 64 576 78.3 576 96L576 256C576 268.9 568.2 280.6 556.2 285.6C544.2 290.6 530.5 287.8 521.3 278.7L464 221.3L310.6 374.6C298.1 387.1 277.8 387.1 265.3 374.6C252.8 362.1 252.8 341.8 265.3 329.3L418.7 176L361.4 118.6C352.2 109.4 349.5 95.7 354.5 83.7zM64 240C64 195.8 99.8 160 144 160L224 160C241.7 160 256 174.3 256 192C256 209.7 241.7 224 224 224L144 224C135.2 224 128 231.2 128 240L128 496C128 504.8 135.2 512 144 512L400 512C408.8 512 416 504.8 416 496L416 416C416 398.3 430.3 384 448 384C465.7 384 480 398.3 480 416L480 496C480 540.2 444.2 576 400 576L144 576C99.8 576 64 540.2 64 496L64 240z"/></svg></a>');
                 }
-                if (pageLink.length > 0 && $(pageLink).parent().find('.review-mode-added').length === 0) {
+                if (pageLink.length > 0 && $(pageLink).parent().find('.review-mode-added').length === 0 && typeof window.screens[i].page.screenshot.src !== 'undefined') {
                     $(pageLink).addClass('review-mode-added');
                     window.getScreenImg(pageLink, window.screens[i].page.screenshot.src);
                 };
@@ -91,11 +91,12 @@
     }
     window.getScreenImg = function(pageLink, url) {
         var token = JSON.parse(localStorage.getItem('Level Access Platform')).accessToken;
-        if (typeof token === 'undefined') return;
         var xhr = new XMLHttpRequest();
         xhr.open('GET', '/api/v1/secure-download/' + url, true);
         xhr.responseType = 'blob';
-        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        if (typeof token !== 'undefined') {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        }
         xhr.onload = function() {
             if (this.status === 200) {
                 var blob = this.response;
